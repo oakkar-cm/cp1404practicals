084391cc105e6a3ef0586b3b5c537b39908c00fb
oakkar-cm <oakkarphyoe@my.jcu.edu.au>
2025-06-01 17:56:15 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,12 @@
-password = input("Enter your password: ")
-length = len(password)
-print("*" * length)
+def main():
+    password = get_password()
+    print("*"* len(password))
+
+def get_password():
+    password = input("Enter password:")
+    while len(password) <= 3:
+        print("Invalid")
+        password = input("Enter password:")
+    return password
+
+main()
